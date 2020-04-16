9f748ed95f7dd94509af73cfc8cc3c5cc5499aa8
Nilomiranda <nilomiranda3@gmail.com>
2020-04-15 22:19:26 -0300

    deploy

--- packages/web/src/common/api.tsx
+++ packages/web/src/common/api.tsx
@@ -5,6 +5,12 @@ function api(tokenRequired = false) {
 
   const api = axios.create({
     baseURL: API_URL,
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://https://dtalk-rest.now.sh',
+      'Allow-Access-Control-Origin': 'https://https://dtalk-rest.now.sh',
+    }
   });
 
   if (tokenRequired) {
