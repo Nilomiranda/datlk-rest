d5e0f5e656afe3c6d074fefd2eb7088ae92d37eb
Nilomiranda <nilomiranda3@gmail.com>
2020-04-16 18:37:17 -0300

    environment for web

--- packages/web/src/common/api.tsx
+++ packages/web/src/common/api.tsx
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
 function api(tokenRequired = false) {
-  const API_URL = 'https://dtalk-api.herokuapp.com/api';
+  let API_URL;
+
+  if (process.env.NODE_ENV === 'development') {
+    API_URL = process.env.REACT_APP_DEV_API_URL;
+  } else if (process.env.NODE_ENV === 'production') {
+    API_URL = process.env.REACT_APP_PROD_API_URL;
+  }
 
   const api = axios.create({
     baseURL: API_URL,
     headers: {
       accept: 'application/json',
       'content-type': 'application/json',
-      'Access-Control-Allow-Origin': 'https://https://dtalk-rest.now.sh',
-      'Allow-Access-Control-Origin': 'https://https://dtalk-rest.now.sh',
+      // 'Access-Control-Allow-Origin': 'https://https://dtalk-rest.now.sh',
+      // 'Allow-Access-Control-Origin': 'https://https://dtalk-rest.now.sh',
     }
   });
 
